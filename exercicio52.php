82f54e3a62a149878dabb7ce71802f738ced039c
DesvG <yagosouza2469@gmail.com>
2026-09-18 17:13:51 -0300

    Add course and student information to exercicio52.php

--- exercicio52.php
+++ exercicio52.php
@@ -1,5 +1,18 @@
 <?php
-
+//*
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programação
+Professor: Flores
+Turma: ESOFT-2
+Componentes:
+[26004871-2] - [Derick Naressi]
+[26013191-2] - [Giovanna Yoko Figueredo Durigan Nakai]
+[26009755-2] - [Guilherme Massaro de Souza]
+[26009484-2] - [Gustavo Zangirolami Pavoni]
+[26007287-2] - [Yago de Souza Rodrigues]
+Data: 19 de Setembro de 2026
+*/
+  
 //*
 Exercício 52:
 Lê um valor em Reais e a cotação do dólar,
